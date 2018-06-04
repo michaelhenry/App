b7298925c4fd30bd2206d9d448c38ea74deb1b6a
Michael Henry Pantaleon <me@iamkel.net>
2018-06-05 03:00:31 +0900

    Initial Commit

---
+++ App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  App
+//
+//  Created by Michael Henry Pantaleon on 2018/06/05.
+//  Copyright © 2018 Michael Henry Pantaleon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
