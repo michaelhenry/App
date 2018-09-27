a522a55762856b08091a39ec33452cbe0a1e2c0a
Michael Henry <me@iamkel.net>
2018-09-28 00:49:18 +0900

    added ci (gitlab-ci) (#3)
    Added fastlane and gitlab-ci configuration

--- AppTests/AppTests.swift
+++ AppTests/AppTests.swift
@@ -22,14 +22,13 @@ class AppTests: XCTestCase {
     }
     
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+       XCTAssert(1 == 1/1)
     }
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measure {
-            // Put the code you want to measure the time of here.
+        
         }
     }
     
